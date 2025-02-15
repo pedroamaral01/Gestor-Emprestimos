b1f0e912e31ed11bee030b335350479a853f3f5b
pedroamaral01 <pedro.estevao@aluno.ufop.edu.br>
2025-02-15 01:32:07 -0300

    estrutura projeto

---
+++ app/Models/Cliente.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $table = 'clientes';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'renda',
+        'documento'
+    ];
+}
