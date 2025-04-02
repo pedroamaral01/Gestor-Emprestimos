3a99e2837c9c48ed93b62be0d88bc64a4f9b6cfc
pedroamaral01 <ph.estevao01@gmail.com>
2025-04-02 13:15:34 -0300

    Alterar rota para o dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('pages.dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', [EmprestimoController::class, 'index'])->name('pages.dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
